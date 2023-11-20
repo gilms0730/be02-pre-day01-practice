db80faa3f4c6d3eeee428c0194d4cc2db7a813d1
gms <giljina333@naver.com>
2023-11-20 16:19:32 +0900

    초안입니다.

---
+++ src/test/Ex03.java
@@ -0,0 +1,59 @@
+package test;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        // 1. 다음과 같이 정사각형을 출력하는 코드 작성
+        /*
+        ----------
+         *****
+         *****
+         *****
+         *****
+         *****
+        ----------
+         */
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+
+        // 2. 다음과 같이 삼각형을 출력하는 코드 작성
+        /*
+        ----------
+         *
+         **
+         ***
+         ****
+         *****
+        ----------
+         */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        // 3. 다음과 같이 삼각형을 출력하는 코드 작성
+        /*
+        ----------
+             *
+            ***
+           *****
+          ********
+         **********
+         ----------
+         *///공백이 414
+
+        int n=5;
+        for (int g = 0; g < n; g = g + 1){
+            for (int m = 0; m < n-g-1; m = m + 1)
+            {System.out.print(" ");}
+            for (int s = 0; s < 2*g+1; s = s + 1)
+            {System.out.print("*");}
+            System.out.println();
+        }
+
+    }
+}
