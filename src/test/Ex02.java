066fe9976bb70a45b3ec2780e0e8882db900e2c7
gms <giljina333@naver.com>
2023-11-20 15:25:23 +0900

    초안입니다.

--- src/test/Ex02.java
+++ src/test/Ex02.java
@@ -5,7 +5,7 @@ public class Ex02 {
         // 다음은 0부터 100까지 숫자 중 하나를 랜덤하게 생성해서
         // num에 저장하는 코드이다.
         //Integer num = (int)(Math.random()*100);
-        for(int i=0;i<6;i++){Integer num = (int)(Math.random()*9);
+        for(int i=0;i<6;i=i+1){Integer num = (int)(Math.random()*9);
             System.out.println(num);}
             // 위 코드를 활용하여 연금 복권 번호를 생성해보시오.
         // 0 ~ 9 숫자 중 6개를 중복 가능
