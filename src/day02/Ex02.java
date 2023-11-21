5f29758b503c0e7afff7afaa5f7bb1f1d6da4c06
gms <giljina333@naver.com>
2023-11-21 10:35:00 +0900

    예쁘게 봐주세요.

--- src/day02/Ex02.java
+++ src/day02/Ex02.java
@@ -6,8 +6,7 @@ public class Ex02 {
         //i가 1부터 100까지 1씩 증가하면서 반봇
         sum =0;
         for(int i=1; i<=100; i=i+1){
-            if (i%2==0){sum= sum + 0;}
-            else {sum=sum+i;}
+            if (i%2!=0){sum= sum+i;}
             }System.out.println(sum);
     }
 }
