fa04763cc4fee6e95945c8a58fafe2beec78f3f3
gms <giljina333@naver.com>
2023-11-21 10:27:47 +0900

    예쁘게 봐주세요.

---
+++ src/day02/Ex02.java
@@ -0,0 +1,13 @@
+package day02;
+public class Ex02 {
+    public static void main(String[] args) {
+        //총합을 저장하기 위한 변수 만들고
+        Integer sum;
+        //i가 1부터 100까지 1씩 증가하면서 반봇
+        sum =0;
+        for(int i=1; i<=100; i=i+1){
+            if (i%2==0){sum= sum + 0;}
+            else {sum=sum+i;}
+            }System.out.println(sum);
+    }
+}
